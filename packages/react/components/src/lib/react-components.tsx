258b1bf1b14837cc54f62ca374bf360bb4f9e74b
Edward Coughlan <ecoughlan@letsgetchecked.com>
2023-11-15 21:52:33 +0000

    vite library update

--- packages/react/components/src/lib/react-components.tsx
+++ packages/react/components/src/lib/react-components.tsx
@@ -1,11 +1,12 @@
 import styles from './react-components.module.css';
+import cn from 'classnames';
 
 /* eslint-disable-next-line */
 export interface ReactComponentsProps {}
 
 export function ReactComponents(props: ReactComponentsProps) {
   return (
-    <div className={styles['container']}>
+    <div className={cn(styles['container'])}>
       <h1>Welcome to ReactComponents!</h1>
     </div>
   );
